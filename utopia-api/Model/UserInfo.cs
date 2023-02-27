7aee605c7f26aa11219e81fde618e2e953e606f1
delguanzon <yodelguanzon@gmail.com>
2023-02-26 22:45:32 -0800

    Create RequestDto:Ongoing

--- utopia-api/Model/UserInfo.cs
+++ utopia-api/Model/UserInfo.cs
@@ -3,7 +3,6 @@ namespace UtopiaApi.Models;
     public class UserInfo
     {
         public int UserInfoId { get; set; }
-        public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string contactNumber { get; set; }
