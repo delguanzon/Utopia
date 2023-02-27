0ece6ea536c06a0906b93719c5128096862267b6
delguanzon <yodelguanzon@gmail.com>
2023-02-26 23:06:21 -0800

    Update ReadmeLogs

--- utopia-api/Model/Request.cs
+++ utopia-api/Model/Request.cs
@@ -5,6 +5,6 @@ public class Request
   public int RequestId { get; set; } 
   public string RequestStatus { get; set; }
   public string RequestDescription { get; set; }
-  public Location Location { get; set; }
-  public User User { get; set; }
+  public int LocationId { get; set; }
+  public UserInfo UserInfo { get; set; }
 }
